fa3ed4981efee6d6711c09d46ac823e3a3d9178e
chkwak@mnlsolution.com <chkwak@mnlsolution.com>
2014-07-23 21:10:21 +0900

    Stack 알고리즘..

---
+++ algoTest/src/main/java/com/chime/algo/ArrayStack.java
@@ -0,0 +1,101 @@
+package com.chime.algo;
+
+/**
+ * 스택 클래스 
+ * http://hyeonstorage.tistory.com/262 참조 
+ * @author chkwak
+ *
+ */
+public class ArrayStack {
+	
+	private int maxSize;
+	private int top;
+	private Object[] stackArray;  
+	
+	
+	public ArrayStack(int maxSize) {
+		
+		this.maxSize = maxSize;
+		this.stackArray = new Object[maxSize]; 
+		this.top = -1; 
+	}
+	
+	
+	
+	/**
+	 * 스택이 비었는지 체크 
+	 * @return
+	 */
+	public boolean empty() {
+		return (top == -1);  
+	}
+	
+	
+	/**
+	 * 스택이 가득 찼는지 체크 
+	 * @return
+	 */
+	public boolean full() {
+		return (top == maxSize -1); 
+	}
+	
+	
+
+	/**
+	 * 스택을 쌓는다.
+	 * @param item
+	 */
+	public void push(Object item) {
+		// 스택이 가득 찼다면.. 
+		if(full()) throw new ArrayIndexOutOfBoundsException((top +1) + ">=" + maxSize);  
+		
+		// 스택 객체 증가
+		stackArray[++top] = item; 	
+	}
+	
+	
+	/**
+	 * 스택 가장 상단 객체 리턴 
+	 * @return
+	 */
+	public Object peek() {
+
+		 if(empty()) throw new ArrayIndexOutOfBoundsException(top);   
+		
+		// Top에 위치한 객체 리턴.. 
+		return stackArray[top]; 
+		
+	}
+	
+	
+	/**
+	 * 가장 위의 데이터 제거 
+	 * @return
+	 */
+	public Object pop() { 
+		
+		/// 제일 상단 객체를 선택 
+		Object item = peek(); 
+		top--; 
+		return item; 
+	}
+
+
+
+	public int getMaxSize() {
+		return maxSize;
+	}
+
+
+
+	public void setMaxSize(int maxSize) {
+		this.maxSize = maxSize;
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ algoTest/src/main/java/com/chime/test/StackTest.java
@@ -0,0 +1,40 @@
+package com.chime.test;
+
+import com.chime.algo.ArrayStack;
+
+public class StackTest {
+
+	public static void main(String[] args) {
+
+		ArrayStack arrayStack = new ArrayStack(5);
+
+		System.out.println("스택 테스트");
+
+		for (int i = 0; i < arrayStack.getMaxSize(); i++) {
+			arrayStack.push("ArrayStack Data " + i);
+		}
+
+		System.out.println(arrayStack.pop());
+		System.out.println(arrayStack.pop());
+		System.out.println(arrayStack.peek());
+		System.out.println(arrayStack.peek());
+		System.out.println(arrayStack.pop());
+		System.out.println(arrayStack.pop());
+		System.out.println(arrayStack.pop());
+		
+		try {
+			for (int i = 0; i < 10; i++) {
+				arrayStack.push("ArrayStack Data " + i);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		System.out.println(arrayStack.pop());
+		System.out.println(arrayStack.pop());
+		System.out.println(arrayStack.pop());
+		System.out.println("==========");
+		
+	}
+
+}
