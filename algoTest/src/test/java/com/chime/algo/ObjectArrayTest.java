7aea8b651932601e1786f374c3ddb130ca223823
nchime@gmail.com <nchime@gmail.com>
2014-07-27 09:21:07 +0900

    객체 Array 정리

---
+++ algoTest/src/test/java/com/chime/algo/ObjectArrayTest.java
@@ -0,0 +1,19 @@
+package com.chime.algo;
+
+public class ObjectArrayTest {
+
+	public static void main(String[] args) {
+		
+		ObjectArray oa = new ObjectArray(-1);
+		
+		oa.add("test1");
+		oa.add("test2");		
+		oa.add("test3");
+		oa.remove("test2");
+		
+		System.out.println(oa.toString()); 
+		
+		
+	}
+
+}
